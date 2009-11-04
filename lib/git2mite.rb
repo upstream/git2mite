16575626dfa559349974a01f9fb09cab5ed8d7c7
Thilo Utke <thilo@upstream-berlin.com>
2009-11-05 02:10:04 +0800

    made a class of git2mite and use instance variables, this fixes missing gui variable for get api key
    changed required rails version to 1.8.7

--- lib/git2mite.rb
+++ lib/git2mite.rb
@@ -9,50 +9,55 @@ require 'mite_client'
 require 'gui'
 require 'git_repo'
 
+class Git2Mite
+  def configuration
+    @configuration ||= Configuration.new
+  end
 
-def configuration
-  @configuration ||= Configuration.new
-end
+  def get_api_key
+    configuration.api_key || configuration.api_key = @gui.ask('What is your api key?')
+  end
 
-def get_api_key
-  configuration.api_key || configuration.api_key = gui.ask('What is your api key?')
-end
+  def check_if_git_repo!
+    @gui.error "Please change to a directory that is a git repository." unless @repo.is_git_repo?
+  end
 
-def check_if_git_repo!(repo, gui)
-  gui.error "Please change to a directory that is a git repository." unless repo.is_git_repo?
-end
+  def check_ruby_version!
+    @gui.error "Sorry you need Ruby 1.8.7+ for this." if RUBY_VERSION < '1.8.7'
+  end
+  
+  
+  def initialize
+    @gui = Gui.new
+    @repo  = GitRepo.new
 
-def check_ruby_version!(gui)
-  gui.error "Sorry you need Ruby 1.9 for this." if RUBY_VERSION < '1.9.1'
-end
+    @gui.print_welcome
+    check_if_git_repo!
+    check_ruby_version!
+    client = MiteClient.new('http://upstream.mite.yo.lk', get_api_key)
+    project_id = @gui.get_project_id(client.projects)
+    user_id = @gui.get_user_id(User.all(client))
+    start_date = @gui.get_date('start date')
+    end_date = @gui.get_date('end date')
+    commits = @repo.commits start_date, end_date
+    author = @gui.get_author commits.map{|date, message, _author| _author}.uniq
 
-gui = Gui.new
-repo  = GitRepo.new
+    puts
+    puts 'Writing commits to mite'
 
-gui.print_welcome
-check_if_git_repo!(repo, gui)
-check_ruby_version!(gui)
-client = MiteClient.new('http://upstream.mite.yo.lk', get_api_key)
-project_id = gui.get_project_id(client.projects)
-user_id = gui.get_user_id(User.all(client))
-start_date = gui.get_date('start date')
-end_date = gui.get_date('end date')
-commits = repo.commits start_date, end_date
-author = gui.get_author commits.map{|date, message, _author| _author}.uniq
-
-puts
-puts 'Writing commits to mite'
-
-commits.each do |date, message, _author|
-  next unless _author == author
-  entries = client.time_entries project_id, user_id, date
-  if entries.empty?
-    STDERR.puts "WARN no time entries for commit #{date.to_s}: #{message}"
-  else
-    entry = entries[rand(entries.size)]['time_entry']
-    client.add_message_to_entry entry, message
-    print "."
+    commits.each do |date, message, _author|
+      next unless _author == author
+      entries = client.time_entries project_id, user_id, date
+      if entries.empty?
+        STDERR.puts "WARN no time entries for commit #{date.to_s}: #{message}"
+      else
+        entry = entries[rand(entries.size)]['time_entry']
+        client.add_message_to_entry entry, message
+        print "."
+      end
+    end
+    puts 'done'
   end
 end
 
-puts 'done'
+Git2Mite.new
